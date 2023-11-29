1701be64602fb2c5b42f22590346009207aa2ab2
Benjamin Manns <benmanns@gmail.com>
2023-11-29 13:28:40 -0500

    Remove required: false from in: query parameters
    This is the default and doesn't need to be specified

--- process_spec.rb
+++ process_spec.rb
@@ -3,6 +3,19 @@
 require 'yaml'
 
 def process(spec)
+  spec['paths'].each do |path, methods|
+    methods.each do |method, details|
+      if details['parameters']
+        details['parameters'].each do |parameter|
+          if parameter['in'] == 'query' && parameter['required'] == false
+            # in: query implies required: false, so we can remove it
+            parameter.delete('required')
+          end
+        end
+      end
+    end
+  end
+
   spec
 end
 
