cbbe009f1e1fec41c05e44a00ecae06c151e7f67
Benjamin Manns <benmanns@gmail.com>
2023-11-29 11:21:17 -0500

    Set up Nix, Rust, openapi-generator-cli toolchain

---
+++ flake.nix
@@ -0,0 +1,48 @@
+{
+  description = "API Client for Metaculus";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          overlays = [ (import rust-overlay) ];
+          pkgs = import nixpkgs {
+            inherit system overlays;
+          };
+          rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+          craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+          src = craneLib.cleanCargoSource ./.;
+          nativeBuildInputs = with pkgs; [ rustToolchain ];
+          buildInputs = with pkgs; [
+            openapi-generator-cli
+          ] ++ lib.optionals stdenv.isDarwin [
+            darwin.apple_sdk.frameworks.Security
+          ];
+        in
+        with pkgs; {
+          devShells.default = mkShell
+            {
+              inherit nativeBuildInputs buildInputs;
+              RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+            };
+        }
+      );
+}
